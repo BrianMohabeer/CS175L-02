d2411979675db1ed72f14afb0fe255d00382994b
BrianMohabeer <114097329+BrianMohabeer@users.noreply.github.com>
2023-02-07 11:17:25 -0500

    version 2

--- restaurant.py
+++ restaurant.py
@@ -2,24 +2,29 @@
 #Brian Mohabeer
 #restaurant
 
-vegetarian = False
-vegan = False
-gluten_free = False
-response1 = input("Is anyone in your party a vegentarian?: ")
-if response1 == "yes":
-    vegetarian = True
-response2 = input("Is anyone in your party a vegan?: ")
-if response2 == "yes":
-    vegan = True
-response3 = input("Is anyone in your party gluten free?: ")
-if response3 == "yes":
-    gluten_free = True
-print("Here are your restaurant choices:")
-if not vegetarian and not vegan and not gluten_free == True:
-    print("Joe's Gourmet Butgers")
-if not vegan and not gluten_free == True:
-    print("Mama's Fine Italian")
-if not vegan == True:
-    print("Main Street Pizza Company")
-print("Corner Cafe")
-print("Chef's Kitchen")
+keep_going = "yes"
+
+while keep_going == "yes":
+    vegetarian = False
+    vegan = False
+    gluten_free = False
+    response1 = input("Is anyone in your party a vegentarian?: ")
+    response2 = input("Is anyone in your party a vegan?: ")
+    response3 = input("Is anyone in your party gluten free?: ")
+    if response1 == "yes":
+        vegetarian = True
+    if response2 == "yes":
+        vegan = True
+    if response3 == "yes":
+        gluten_free = True
+    print("Here are your restaurant choices:")
+    if not vegetarian and not vegan and not gluten_free == True:
+        print("Joe's Gourmet Butgers")
+    if not vegan and not gluten_free == True:
+        print("Mama's Fine Italian")
+    if not vegan == True:
+        print("Main Street Pizza Company")
+        print("Corner Cafe")
+        print("Chef's Kitchen")
+    keep_going = input("Enter 'yes' if you would like to do another" +
+                       " restaurant search (enter 'no' to end): ")
